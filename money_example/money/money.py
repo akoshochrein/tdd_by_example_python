c5259fd1c3124f308d79f1aeeaec3ebd385d9fae
akoskaaa <akos.hochrein@prezi.com>
2014-05-02 17:56:05 +0200

    moving toward times in money 2

--- money_example/money/money.py
+++ money_example/money/money.py
@@ -1,30 +1,50 @@
 
-class Money(object):
+import abc
 
-    def __init__(self, amount):
+
+class Money(object):
+    __metaclass__ = abc.ABCMeta
+
+    def __init__(self, amount, currency):
         self._amount = amount
+        self._currency = currency
 
     @property
     def amount(self):
         return self._amount
 
+    def currency(self):
+        return self._currency
+
     def equals(self, money):
         return self._amount == money.amount and type(self) == type(money)
+
+    @abc.abstractmethod
+    def times(self, multiplier):
+        return
+
+    @staticmethod
+    def dollar(amount):
+        return Dollar(amount, 'USD')
+
+    @staticmethod
+    def franc(amount):
+        return Franc(amount, 'CHF')
 
 
 class Dollar(Money):
 
-    def __init__(self, amount):
-        super(Dollar, self).__init__(amount)
+    def __init__(self, amount, currency):
+        super(Dollar, self).__init__(amount, currency)
 
     def times(self, multiplier):
-        return Dollar(self._amount * multiplier)
+        return Money.dollar(self._amount * multiplier)
 
 
 class Franc(Money):
 
-    def __init__(self, amount):
-        super(Franc, self).__init__(amount)
+    def __init__(self, amount, currency):
+        super(Franc, self).__init__(amount, currency)
 
     def times(self, multiplier):
-        return Franc(self._amount * multiplier)
+        return Money.franc(self._amount * multiplier)
