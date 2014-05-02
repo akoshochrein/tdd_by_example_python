36b59747b7f16e66940f157dfc4430d6917390e7
akoskaaa <akos.hochrein@prezi.com>
2014-05-02 17:11:17 +0200

    added franc

--- money_example/money/money.py
+++ money_example/money/money.py
@@ -2,7 +2,30 @@
 class Dollar(object):
 
     def __init__(self, amount):
-        self.amount = amount
+        self._amount = amount
+
+    @property
+    def amount(self):
+        return self._amount
 
     def times(self, multiplier):
-        self.amount *= multiplier
+        return Dollar(self._amount * multiplier)
+
+    def equals(self, dollar):
+        return self._amount == dollar.amount
+
+
+class Franc(object):
+
+    def __init__(self, amount):
+        self._amount = amount
+
+    @property
+    def amount(self):
+        return self._amount
+
+    def times(self, multiplier):
+        return Franc(self._amount * multiplier)
+
+    def equals(self, franc):
+        return self._amount == franc.amount
--- money_example/tests/test_money.py
+++ money_example/tests/test_money.py
@@ -1,6 +1,6 @@
 
 import unittest2
-from money.money import Dollar
+from money.money import Dollar, Franc
 
 
 class TestMoney(unittest2.TestCase):
@@ -8,10 +8,21 @@ class TestMoney(unittest2.TestCase):
     def setUp(self):
         pass
 
-    def test_multiplication(self):
+    def test_dollar_multiplication(self):
         five = Dollar(6)
-        five.times(2)
-        self.assertEquals(12, five.amount)
+        self.assertTrue(Dollar(12).equals(five.times(2)))
+        self.assertTrue(Dollar(30).equals(five.times(5)))
+
+    def test_franc_multiplication(self):
+        five = Franc(6)
+        self.assertTrue(Franc(12).equals(five.times(2)))
+        self.assertTrue(Franc(30).equals(five.times(5)))
+
+    def test_dollar_equality(self):
+        self.assertTrue(Dollar(42).equals(Dollar(42)))
+
+    def test_dollar_inequality(self):
+        self.assertFalse(Dollar(42).equals(Dollar(69)))
 
 if __name__ == '__main__':
     unittest2.main()
